64f9a42f25c5f73cade5c6968ceb2e40f1d7f18a
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2023-05-04 17:12:15 +0200

    init

---
+++ src/wallet/wrap_service.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023-2024 Infinium Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#define BC_WRAP_SERVICE_ID                                "W"
+
+#define BC_WRAP_SERVICE_INSTRUCTION_ERC20                 "ERC20"  //erc20 wrap operation
+#define BC_WRAP_SERVICE_INSTRUCTION_UNWRAP                "UNWRAP"  //erc20 unwrap operation
+
+
+#define BC_WRAP_SERVICE_CUSTODY_WALLET                    ""
