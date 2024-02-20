279593f77228fc20b6c5c2316671457556eb794f
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-20 16:21:27 +0100

    init

--- utils/buildings/linux/64bit/Infinium.sh
+++ utils/buildings/linux/64bit/Infinium.sh
@@ -3,6 +3,7 @@ script_dir=$( dirname "$(readlink -f "$0")" )
 
 export LD_LIBRARY_PATH=$script_dir/lib
 export QT_PLUGIN_PATH=$script_dir/lib
+export QTWEBENGINE_DISABLE_SANDBOX=1
 
 echo $LD_LIBRARY_PATH
 echo $QT_PLUGIN_PATH
