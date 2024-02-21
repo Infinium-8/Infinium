5a23859c7931ddb8aeaab1e434ec2f65b084eede
Infinium-8 <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-21 23:46:18 +0100

    init

--- src/gui/qt-daemon/application/notification_helper.mm
+++ src/gui/qt-daemon/application/notification_helper.mm
@@ -1,17 +1,13 @@
 #include "notification_helper.h"
-#import <Foundation/Foundation.h>
-#import <Foundation/NSUserNotification.h>
-#import <Foundation/NSUserNotificationCenter.h>
-#import <Foundation/NSString.h>
+#include <Foundation/NSString.h>
+#include <Foundation/NSUserNotification.h>
 
 void notification_helper::show(const std::string& title, const std::string& message)
 {
-    NSUserNotification *userNotification = [[NSUserNotification alloc] init];
-    [userNotification setTitle:[NSString stringWithUTF8String:title.c_str()]];
-    [userNotification setInformativeText:[NSString stringWithUTF8String:message.c_str()]];
+  NSUserNotification *userNotification = [[[NSUserNotification alloc] init] autorelease];
+  userNotification.title = [NSString stringWithUTF8String:title.c_str()];
+  userNotification.informativeText =  [NSString stringWithUTF8String:message.c_str()];
 
-    NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
-    [center deliverNotification:userNotification];
-
-    [userNotification release]; // Liberar el objeto
+  NSUserNotificationCenter* center = [NSUserNotificationCenter defaultUserNotificationCenter];
+  [center deliverNotification:userNotification];
 }
