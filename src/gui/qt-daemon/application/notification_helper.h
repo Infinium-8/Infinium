5692c995ee31377f4b256d2819d99186aa6bd5ef
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-22 01:50:37 +0100

    Update notification_helper.h

--- src/gui/qt-daemon/application/notification_helper.h
+++ src/gui/qt-daemon/application/notification_helper.h
@@ -9,11 +9,11 @@
 
 struct notification_helper
 {
-  static void show(const std::string& title, const std::string& message)
+  static void show(const std::string& title, const std::string& message);
   #if !defined(__APPLE__)
     {
       // just a stub, implemented for macOS only, see .mm
       LOG_PRINT_RED("system notifications are supported only for macOS!", LOG_LEVEL_0);
     }
-  #endif;
+  #endif
 };
